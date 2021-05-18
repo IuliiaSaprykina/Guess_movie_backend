0946eb8217b772ba6742f3bafc46e764e23cd0fd
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2021-05-18 18:02:13 -0400

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -5,26 +5,11 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    database: {
-      type: "postgres",
-      host: "ec2-34-206-8-52.compute-1.amazonaws.com",
-      port: 5432,
-      username: "yhzjtxcbiyvycy",
-      password:
-        "8558519dadee5d3c3a0dea94db0108dcbdec286ed1b46571b4c42c6698241641",
-      database: "d5tie78s3pvvq8",
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
+    connection: process.env.DATABASE_URL,
+    ssl: { 
+      require: true,
+      rejectUnauthorized: false 
     },
-    // connection: process.env.DATABASE_URL,
-    // ssl: { 
-    //   require: true,
-    //   rejectUnauthorized: false 
-    // },
     migrations: {
         directory: './migrations',
     },
