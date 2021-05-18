667bb88ddcfd900284d1e25868c7468a6695758d
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2021-05-17 20:20:56 -0400

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -3,4 +3,12 @@ module.exports = {
     client: 'pg',
     connection: "postgres:///questions",
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 };
